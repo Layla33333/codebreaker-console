2913f0b35d54ac4e94a2c72a7c46533dce62a56e
layla <lkesssler3@cnm.edu>
2021-10-06 15:16:47 -0600

    code game entering

---
+++ src/main/java/edu/cnm/deepdivecodebreaker/Application.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdivecodebreaker;
+
+public class Application {
+
+  public static void main(String[] args) {
+    // TODO Read command-line arguments for pool and length.
+    // TODO Make request to code breaker service to start game with a new code.
+    // TODO while code is not guessed:
+    // 1.Read guess from user input.
+    // 2.Submit guess to codebreaker service.
+    // 3.Display guess results
+  }
+
+}
