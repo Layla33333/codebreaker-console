39563a54456a554fae27664efa2a29756e14fbea
layla <lkesssler3@cnm.edu>
2021-10-07 11:04:28 -0600

    added repository to manage service interaction.

--- src/main/java/edu/cnm/deepdivecodebreaker/Application.java
+++ src/main/java/edu/cnm/deepdivecodebreaker/Application.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdivecodebreaker;
 
 import edu.cnm.deepdivecodebreaker.model.Game;
+import edu.cnm.deepdivecodebreaker.service.GameRepository;
 import edu.cnm.deepdivecodebreaker.service.WebServiceProxy;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -9,14 +10,46 @@ import retrofit2.Response;
 
 public class Application {
 
+  private static final String DEFAULT_POOL = "ABCDEF";
+  private static final int DEFAULT_LENGTH = 3;
+
+  // static non-final
+
+  private final GameRepository repository;
+  private Game game;
+
+  // This is a constructor
+  private Application(String[] args) throws IOException {
+    String pool = DEFAULT_POOL;
+    int length = DEFAULT_LENGTH;
+
+    switch (args.length) {
+      default:
+        // Deliberate fall through!!
+      case 2:
+        length = Integer.parseInt(args[1]);
+        // Deliberate fall through!
+      case 1:
+        pool = args[0];
+      case 0:
+        // Do Nothing!!!
+
+    }
+
+    repository = new GameRepository();
+    startGame(pool, length);
+
+
+  }
+
+  private void startGame(String pool, int length) throws IOException {
+    game = repository.startGame(pool, length);
+  }
+
+
   public static void main(String[] args) throws IOException {
 
-
-    // TODO Read command-line arguments for pool and length.
-  String pool = "ABCDEF";//FIXME Read from args
-  int length = 3; // FIXME Read From args
-  Game game = startGame(pool, length);
-    System.out.printf("Game id = %s%n", game.getId());
+    Application application = new Application(args);
 
 
     // TODO while code is not guessed:
@@ -25,16 +58,4 @@ public class Application {
     // 3.Display guess results
   }
 
-  private static Game startGame(String pool, int length) throws IOException {
-    WebServiceProxy proxy = WebServiceProxy.getInstance();
-    Game game = new Game();
-    game.setPool(pool);
-    game.setLength(length);
-    Call<Game> call = proxy.startGame(game);
-    Response<Game> response = call.execute();
-    if (!response.isSuccessful()) {
-      throw new RuntimeException(response.message());
-    }
-      return response.body();
-  }
 }
